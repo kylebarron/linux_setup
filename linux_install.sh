1ff3a66699ad9f82117722c37c133e783c3d8d0a
Kyle Barron <barronk@mit.edu>
2017-06-25 18:16:26 -0400

    updated

--- linux_install.sh
+++ linux_install.sh
@@ -189,6 +189,10 @@ sudo apt-get install gnome-themes-standard gnome-tweak-tool pixmap
 curl -sL https://github.com/nana-4/Flat-Plat/archive/v20170605.tar.gz | tar xz
 cd Flat-Plat-20170605 && sudo ./install.sh
 
+# Install Ruby and Jekyll
+sudo apt-get install ruby-full
+sudo gem install jekyll
+
 
 # Install CUDA
 wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
