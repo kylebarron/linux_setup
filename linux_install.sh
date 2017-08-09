b0c2f4d501961dbbeb93402736ec6be4724db9d3
Kyle Barron <barronk@mit.edu>
2017-08-09 16:49:59 -0400

    Install Virtualbox extensions

--- linux_install.sh
+++ linux_install.sh
@@ -124,6 +124,8 @@ sudo gem install bundler
 # Install VirtualBox
 wget http://download.virtualbox.org/virtualbox/5.1.26/virtualbox-5.1_5.1.26-117224~Ubuntu~xenial_amd64.deb
 sudo dpkg -i virtualbox-5.1_5.1.26-117224-Ubuntu-xenial_amd64.deb
+# Download VirtualBox Extension Pack
+wget http://download.virtualbox.org/virtualbox/5.1.26/Oracle_VM_VirtualBox_Extension_Pack-5.1.26-117224.vbox-extpack
 
 # Install SSH Server
 sudo apt install -y openssh-server
