ca841803d7b278c80925aa0e469c8e841ad0e4b5
Kyle Barron <barronk@mit.edu>
2017-08-09 12:40:27 -0400

    Update upon new linux install

--- linux_install.sh
+++ linux_install.sh
@@ -26,10 +26,10 @@ cp dotfiles/zsh/zshrc_desktop ~/.zshrc
 cp dotfiles/bash/bashrc_desktop ~/.bashrc
 
 # Install Anaconda
-wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
-sudo bash Anaconda3-4.3.1-Linux-x86_64.sh -b -p /opt/anaconda
-rm Anaconda3-4.3.1-Linux-x86_64.sh
-echo 'export PATH="/opt/anaconda/bin:$PATH"' >> ~/.zshrc
+wget https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
+sudo bash Anaconda3-4.4.0-Linux-x86_64.sh -b -p /opt/anaconda
+rm Anaconda3-4.4.0-Linux-x86_64.sh
+sudo chown -R $USER:$USER /opt/anaconda
 
 # Install R
 sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu xenial/"
@@ -55,35 +55,37 @@ sudo ./install_packages.R
 rm install_packages.R
 
 # Install RStudio Desktop
-wget https://download1.rstudio.org/rstudio-1.0.143-amd64.deb
+wget https://download1.rstudio.org/rstudio-1.0.153-amd64.deb
+sudo apt install libjpeg62 libgstreamer0.10-0 libgstreamer-plugins-base0.10-0
 sudo apt -fy install
-sudo dpkg -i rstudio-1.0.143-amd64.deb
-rm rstudio-1.0.143-amd64.deb
+sudo dpkg -i rstudio-1.0.153-amd64.deb
+rm rstudio-1.0.153-amd64.deb
 # Replace RStudio settings
+mkdir -p ~/.rstudio-desktop/monitored/user-settings/
 cp dotfiles/rstudio/user-settings ~/.rstudio-desktop/monitored/user-settings/user-settings
 
 # Install RStudio Server
 sudo apt install -y gdebi-core
-wget https://download2.rstudio.org/rstudio-server-1.0.143-amd64.deb
-sudo gdebi --n rstudio-server-1.0.143-amd64.deb
+wget https://download2.rstudio.org/rstudio-server-1.0.153-amd64.deb
+sudo gdebi --n rstudio-server-1.0.153-amd64.deb
 echo "www-address=127.0.0.1" | sudo tee --append /etc/rstudio/rserver.conf
 #sudo rstudio-server restart
 sudo rstudio-server start
-rm rstudio-server-1.0.143-amd64.deb
+rm rstudio-server-1.0.153-amd64.deb
 
 # Install Node.js and npm
 # Needed to install term3 as an Atom package
 cd /opt
-sudo wget https://nodejs.org/dist/v6.10.3/node-v6.10.3-linux-x64.tar.xz
-sudo tar xvfJ node-v6.10.3-linux-x64.tar.xz
-sudo rm node-v6.10.3-linux-x64.tar.xz
-sudo mv node-v6.10.3-linux-x64 node
-echo 'export PATH="/opt/node/bin:$PATH"' >> ~/.zshrc
+sudo wget https://nodejs.org/dist/v6.11.2/node-v6.11.2-linux-x64.tar.xz
+sudo tar xvfJ node-v6.11.2-linux-x64.tar.xz
+sudo rm node-v6.11.2-linux-x64.tar.xz
+sudo mv node-v6.11.2-linux-x64 node
 cd $cwd
 
 # Install Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install libappindicator1
 sudo apt -fy install
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 # doesn't work on headless remote linux machine because audio/visual drivers not installed
 rm google-chrome-stable_current_amd64.deb
@@ -119,6 +121,10 @@ sudo gem install jekyll
 sudo gem install bundler
 # Need to run (sudo) bundle install or bundle update in website folder to install other dependent gems.
 
+# Install VirtualBox
+wget http://download.virtualbox.org/virtualbox/5.1.26/virtualbox-5.1_5.1.26-117224~Ubuntu~xenial_amd64.deb
+sudo dpkg -i virtualbox-5.1_5.1.26-117224-Ubuntu-xenial_amd64.deb
+
 # Install SSH Server
 sudo apt install -y openssh-server
 
@@ -128,8 +134,7 @@ sudo chmod 755 /etc/ssh/$(whoami)
 #sudo mv ~/.ssh/authorized_keys /etc/ssh/$(whoami)/authorized_keys
 sudo touch /etc/ssh/$(whoami)/authorized_keys
 sudo chmod 644 /etc/ssh/$(whoami)/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC9qYxWBMPHc22t7gEa1mbDPlBkISDXGYGsJt9z6skeQqzZnqrqhqoXOQUXTSwJm5p4+1gCPR63KjwKPp5rAIKNuNKAM1Cf+1RDyLu1I6Ixtf6fv5NtWr1oK2VwTyMP+rPvaYAaNpr5aMF6JqJGt37/qVY9hyJu2D6p2tw5N/fzgtALeK820qeposmfb5DCn90zsDyTHi7khpp5nO9WfCFXFYO87Vxk2pzeMYSwmiabHdL6Tqs7jjNC3XLuOxaB9drv0J+PcNLclFEjeLq0dGWPleIGmjEx2xi7xWBsmYM2bnpADcuAMd25xpICf4rkr/tVlIbuiy8nUli9p6wiK8PKMcwxVs8+kVD+VuEmqTPKdNnk2zYSLkkEZSyo+X1wReXq56rtRJQWhpvFMvEUMbne4oHk90QYpVgMXZSiNF4h6srQq1yeSnV72UTmr9M2cgYkQXw9j0oB93Yoh8mpZIgCn9wBHWGljMURyR28Le1/dAPJvFSri6HhcV5+jy54g2q5PxlLNcA2AZneh5VW88VnW0iNrS9gN/Btk1EumcD3x2+M4XCdNhTzI548WFBEDNF2TO4VcEZIsqIsScWdoK6o/A7Bp2k5Pxj0RTkbB8jX5VbCwWrNQuY7ZPiiGBJchR18agWJDtlSV2AzvXW0tiAYG8yapBgvdzn3L9lvJ0vQnw== kyle@mac-linux" | sudo tee --append /etc/ssh/$(whoami)/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLj+wWR7tstScXy6vDi7NmMqSu40tiF1Qa6OLIqtMVCRRvxhu+AAL6Ig/7wcPz1A0vwgMbpahfNq//FgyHIZLiYz+ysrrfUZxhBr6jQMXe+BDugZTBV9epsVxw3qzHvI4X04/sT7v0mybQb4MLWZ8rpeJLChKxASMO02JPLIi6tK9qlh+SMJFld+joMZTa8uxaBLipWSJL2F3kEZveA7/lAWqz/Kj6zERTVUb6kxSDujCRCP2oPOP3/546/1iwSI8pmo1WXifStAmJ5DQp0xBGdn495zVV5RVyqz2+qUnN7LKg+U3AqJdKmBHT9CDGm9+nBagSXoO8CRx/mmmQR3rp kyle@mac-osx" | sudo tee --append /etc/ssh/$(whoami)/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDqIiTcGgYb0DHwLG+WQXgt7t+pBFYdyO494VFFsv5KFa1g+pqdcvZyPoyVqV2ZT2h5w055gKau+GLbtavyK74GRDJFYBNomuEIXp9RBWTt6/qzpnMKTvmMAKgLTujIrUjtYbVhncUB7mV438FecbnNBQW61jqCkEQQSwUlli94RD3C+qOjnLIe9vrIlvcYbZMZUfCmL7VUQByJlkvfhpiteRzXfpXamuCgQAn8GiE9c9S1EFkqcT/7ECLkJNL8ToNVDU7DieQP1ZIIPy6ktG3EOYAcmJwVQ3kSYJcQqL8cy4PVHrZuLyKefKrqeRaSFs1uA83DpjOCxfSBmmqBMR9kLAdG+rkA+a8/Fjn6BPyab6Kr0Uxy0LJfHGgUGA5hKwZExfLzioSIXH9veHUETOcUhG4fmhCWuRGD2ZW2231R/s9ZVjZrdkzCoIrrcnhN4LrnQb29aP15V3RH6hJhWPG8e+paOfIvW8zQaQoqPf9exGhV+CaPPh3OqLKPU1qSZDjyShb4GxKqCJz3ScKIf+bAi+8T/rvQVsw3gLzc+kD9yLdbX30HIUI5sQdyYZAKVNfpuWgIe9e7Q1DVZP3IeBot5GZyTUave7FpTum4TPxc3vUn5ktz7HRMt03Ff64hV3b5RMJbV8s2zaoMUyid79wNUGU2AZAxOWjnVZuaIzPtXw== kyle@mac.local" | sudo tee --append /etc/ssh/$(whoami)/authorized_keys
 sudo sed -i 's@#AuthorizedKeysFile@AuthorizedKeysFile@g' /etc/ssh/sshd_config
 sudo sed -i 's@%h/.ssh/authorized_keys@/etc/ssh/%u/authorized_keys@g' /etc/ssh/sshd_config
 sudo sed -i 's@#PasswordAuthentication yes@PasswordAuthentication no@g' /etc/ssh/sshd_config
@@ -150,6 +155,8 @@ sudo chmod 755 /usr/bin/rclone
 sudo mkdir -p /usr/local/share/man/man1
 sudo cp rclone.1 /usr/local/share/man/man1/
 sudo mandb
+cd $cwd
+rm -r rclone-v1.37-linux-amd64
 
 # Fuzzy File Finder
 cd $cwd
@@ -160,6 +167,8 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 sudo apt install -y filezilla
 
 # Install Google Earth
+sudo apt install lsb-core
+sudo apt -fy install
 wget https://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
 sudo dpkg -i google-earth-stable_current_amd64.deb
 rm google-earth-stable_current_amd64.deb
@@ -169,13 +178,13 @@ sudo apt install -y vlc
 
 # Install Julia
 cd /opt
-sudo wget https://julialang-s3.julialang.org/bin/linux/x64/0.5/julia-0.5.2-linux-x86_64.tar.gz
-sudo tar -xzf julia-0.5.2-linux-x86_64.tar.gz
-sudo mv julia-f4c6c9d4bb/ julia/
-sudo rm julia-0.5.2-linux-x86_64.tar.gz
-echo 'export PATH="/opt/julia/bin:$PATH"' >> ~/.zshrc
+sudo wget https://julialang-s3.julialang.org/bin/linux/x64/0.6/julia-0.6.0-linux-x86_64.tar.gz
+sudo tar -xzf julia-0.6.0-linux-x86_64.tar.gz
+sudo mv julia-903644385b/ julia/
+sudo rm julia-0.6.0-linux-x86_64.tar.gz
 cd $cwd
 
+# Install Docker
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -197,6 +206,7 @@ python -m bash_kernel.install
 curl -O https://prerelease.keybase.io/keybase_amd64.deb
 sudo dpkg -i keybase_amd64.deb
 sudo apt install -fy
+rm keybase_amd64.deb
 
 # Install QGIS
 sudo add-apt-repository "deb http://qgis.org/ubuntugis-ltr xenial main/"
@@ -208,11 +218,12 @@ sudo apt update
 sudo apt install -y qgis python-qgis qgis-plugin-grass
 
 # Install Atom
-wget https://atom-installer.github.com/v1.17.2/atom-amd64.deb
+wget https://github.com/atom/atom/releases/download/v1.19.0/atom-amd64.deb
 sudo dpkg -i atom-amd64.deb
 rm atom-amd64.deb
 
 # Restore Atom Settings
+mkdir -p ~/.atom
 cp dotfiles/atom/* ~/.atom/
 # Change Atom Icon to atom-material-ui Icon
 sudo cp dotfiles/atom/atom_icon.png /usr/share/pixmaps/atom_material_ui.png
@@ -228,25 +239,33 @@ sudo apt install pandoc
 # My PATH puts /opt/anaconda/bin before /usr/bin because I want python 3.6 to be automatically sourced
 # But /opt/anaconda/bin has old versions of pandoc and pandoc-citeproc, and I want the newer versions to be used
 # So I'll remove those binaries in /opt/anaconda/bin and symlink it to /usr/bin
-sudo rm /opt/anaconda/bin/pandoc
-sudo rm /opt/anaconda/bin/pandoc-citeproc
+# sudo rm /opt/anaconda/bin/pandoc Doesn't exist?
+# sudo rm /opt/anaconda/bin/pandoc-citeproc
 sudo ln -s /usr/bin/pandoc /opt/anaconda/bin/pandoc
 sudo ln -s /usr/bin/pandoc-citeproc /opt/anaconda/bin/pandoc-citeproc
 
 # autokey-gtk
 sudo add-apt-repository ppa:troxor/autokey
 sudo apt update
-sudo apt install autokey-gtk
+sudo apt install -y autokey-gtk
+mkdir -p ~/.config/autokey/data/Sample\ Scripts/
 cp dotfiles/autokey/run_stata.py          ~/.config/autokey/data/Sample\ Scripts/run_stata.py
 cp dotfiles/autokey/.run_stata.json       ~/.config/autokey/data/Sample\ Scripts/.run_stata.json
 cp dotfiles/autokey/run_stata_chunk.py    ~/.config/autokey/data/Sample\ Scripts/run_stata_chunk.py
 cp dotfiles/autokey/.run_stata_chunk.json ~/.config/autokey/data/Sample\ Scripts/.run_stata_chunk.json
 
-# Install tmux
-sudo apt install tmux
+# Other small things
+sudo apt install -y tmux
+sudo apt install python3-dev python3-pip
+sudo pip3 install thefuck
+wget https://github.com/github/hub/releases/download/v2.2.9/hub-linux-amd64-2.2.9.tgz
+extract hub-linux-amd64-2.2.9.tgz
+cd hub-linux-amd64-2.2.9
+sudo ./install
+
 
 # Flat plat design
-sudo apt install gnome-themes-standard gnome-tweak-tool pixmap
+sudo apt install -y gnome-themes-standard gnome-tweak-tool pixmap
 curl -sL https://github.com/nana-4/Flat-Plat/archive/v20170605.tar.gz | tar xz
 cd Flat-Plat-20170605 && sudo ./install.sh
 
@@ -272,7 +291,7 @@ sudo wget https://www.privateinternetaccess.com/openvpn/openvpn.zip
 sudo apt install unzip
 sudo unzip openvpn.zip
 sudo rm openvpn.zip
-touch pass.txt ## Add username and pasword in here manually
+sudo touch pass.txt ## Add username and pasword in here manually
 sudo chmod 600 pass.txt
 for filename in *.ovpn
 do
