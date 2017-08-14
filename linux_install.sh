560f172a5a0ab18f2d509b7a966389e0b2173471
Kyle Barron <barronk@mit.edu>
2017-08-14 19:57:08 -0400

    add f.lux

--- linux_install.sh
+++ linux_install.sh
@@ -117,6 +117,15 @@ sudo add-apt-repository -y ppa:lyx-devel/release
 sudo apt update
 sudo apt install -y lyx
 
+# Install f.lux
+sudo apt install git python-appindicator python-xdg python-pexpect python-gconf python-gtk2 python-glade2 libxxf86vm1
+git clone "https://github.com/xflux-gui/xflux-gui.git"
+cd xflux-gui
+python download-xflux.py
+sudo python setup.py install
+cd ../
+rm -rf xflux-gui
+
 # Update Git config
 cp dotfiles/git/gitconfig_desktop ~/.gitconfig
 
