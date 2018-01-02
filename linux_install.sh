da2b1607e2bf51267959005d0e6d043a13b6b064
Kyle Barron <kylebarron2@gmail.com>
2018-01-02 17:43:07 -0500

    fix zsh-syntax-highlighting install location

--- linux_install.sh
+++ linux_install.sh
@@ -51,7 +51,7 @@ if [[ $oh-my-zsh = 'True' ]]; then
 fi
 
 if [[ $zsh-syntax-highlighting = 'True' ]]; then
-    sudo apt install -y zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
 if [[ $materialshell = 'True' ]]; then
