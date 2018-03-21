9d8344cc55936f00fc42434f5e34195753eacafc
Kyle Barron <kylebarron2@gmail.com>
2018-03-21 12:13:30 -0400

    add example cli options

---
+++ cli_full.sh
@@ -0,0 +1,126 @@
+#! /usr/bin/env bash
+
+# Set as 'False' if you don't have sudo privileges
+sudo='True'
+
+# Terminal Essentals:
+zsh='True'
+oh-my-zsh='True'
+materialshell='True'
+zsh-autosuggestions='True'
+zsh-syntax-highlighting='True'
+
+# My configuration files
+gitconfig='True'
+zshrc='True'
+bashrc='True'
+privateinternetaccess_config='False'
+
+# Programming Languages
+# Python
+anaconda3='True'
+anaconda2='False'
+miniconda3='False'
+cuda='False'
+
+# R
+r='True'
+r-tidyverse='False'
+r-gis='False'
+r-all='True'
+rstudio-desktop='True'
+rstudio-server='False'
+
+# Julia
+julia='True'
+
+# Stata
+readstat='True'
+
+# NodeJS
+nodejs8='False'
+nodejs9='True'
+
+# Jupyter
+jupyter-notebook-remote='True'
+irkernel='True'
+ijulia='True'
+bash-kernel='True'
+ijavascript='True'
+
+# Databases
+mysql='False'
+postgres='False'
+postgis='False'
+
+# GIS
+qgis='False'
+
+# Text Editors
+atom='True'
+atom-packages='True'
+fira-code='True'
+micro='True'
+sublime-text='False'
+
+# Document processing
+# TeX
+texlive='True'
+texmaker='False'
+lyx='False'
+
+# Desktop programs
+gitkraken='False'
+google-chrome='True'
+google-earth='True'
+spotify='True'
+virtualbox='True'
+
+# Helpful Utilities
+ag='True'
+autokey-gtk='True'
+caprine='True'
+chromedriver='True'
+compizconfig='True'
+docker='True'
+dropbox='True'
+fd='True'
+filezilla='True'
+flat-plat='True'
+fuzzy-file-finder='True'
+gtop='True'
+hub='True'
+jekyll='True'
+jq='True'
+keybase='True'
+lastpass-cli='False'
+libmagick='True'
+mkdocs='True'
+nitrogen='True'
+openvpn='False'
+pandoc='True'
+peek='True'
+pv='True' # measure pipe throughput
+rclone='True'
+redshift='True'
+ripgrep='True'
+shellcheck='True'
+smem='True'
+speed-test='True'
+ssh-server='False'
+thefuck='True'
+tmux='True'
+tree='True'
+vlc='True'
+xclip='True'
+xsel='True'
+xsv='True'
+yapf='True'
+
+### Photography software
+
+darktable='True'
+digikam='False'
+
+source linux_install.sh
+
